da2319af00fe770028c0c149b4d3408ffd066304
Tunan10 <tunansakib200@gmail.com>
2026-02-23 22:53:18 +0600

    completing frontend design

---
+++ script.js
@@ -0,0 +1,4 @@
+// Header count change
+let totalCount = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectCount = document.getElementById('rejected-count');
